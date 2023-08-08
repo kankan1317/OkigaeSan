02a68cea9c754a9e81ddf61537bac7a1b7038b0d
kankan1317 <87018974+kankan1317@users.noreply.github.com>
2023-08-09 00:06:02 +0900

    AnimationFile作成関数を実装

--- Assets/KanKan/OkigaeSan/Editor/Utilty.cs
+++ Assets/KanKan/OkigaeSan/Editor/Utilty.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 
 namespace OkigaeSan
 {
-    public class Utilty
+    public static class Utilty
     {
         public static List<GameObject> GetChildren(GameObject obj)
         {
@@ -18,7 +19,41 @@ namespace OkigaeSan
             }
             return childrens;
         }
-    }
 
+        public static void CreateAnimation(GameObject obj, bool state)
+        {
+            string AssetsPath = "Assets/KanKan/OkigaeSan/Animation/";
+
+            float val = state ? 1f : 0f;
+            string sState = state ? "_On" : "_Off";
+
+            var curve = AnimationCurve.Linear(0, val, (float)1/60, val);
+            var clip = new AnimationClip();
+
+            clip.name = obj.name + sState;
+
+            clip.SetCurve(GetFullPath(obj), typeof(GameObject), "isActive", curve);
+            AssetDatabase.CreateAsset(clip, AssetsPath + clip.name + ".anim");
+        }
+
+        public static string GetFullPath(GameObject obj)
+        {
+            return GetFullPath(obj.transform);
+        }
+
+        public static string GetFullPath(Transform obj)
+        {
+            string path = obj.name;
+            var parent = obj.parent;
+
+            while (parent)
+            {
+                path = $"{parent.name}/{path}";
+                parent = parent.parent;
+
+            }
+            return path ;
+        }
+    }
 }
 
